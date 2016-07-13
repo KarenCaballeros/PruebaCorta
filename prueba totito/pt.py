9b86005eb157988375aa0fcdd3259935352b91d9
KarenCaballeros <anakarencaballeros@gmail.com>
2016-07-12 23:07:25 -0600

    totito

---
+++ prueba totito/pt.py
@@ -0,0 +1,6 @@
+import pt2
+print("Totito")
+print("Bienvenuuu")
+
+listita= [[1,-1,-1],[1,1,-1],[1,1,1]]
+print(pt2.ganadores(listita))
---
+++ prueba totito/pt2.py
@@ -0,0 +1,53 @@
+def ganador(totito):
+		sumas= []
+		MAGIA
+		sumas=[5446545465]
+		if 3 in sumas:
+			return"X Gano"
+		elif -3 in sumas:
+			return "O gano"
+		else:
+			return "Nadie Gano"	
+
+
+def ganar(lista):
+	lista2= []
+	for i in lista:
+		suma= 0
+		for j in i:
+			suma= suma + j
+		print(suma)	
+		lista2.append(suma)
+		print(lista2)
+	
+
+	for i in lista:
+		suma = 0	
+		for j in i:
+			suma= suma + i
+			print(suma)	
+		lista2.append(suma)	
+
+	return lista2
+
+def ganadores(lista):
+	lista2= []
+	for i in lista:
+		for j in i:
+			lista2.append(j)
+	suma = []
+	fila= 0
+	for lista2 in range[0:3]:
+		fila = fila + i
+	print(fila)
+	suma.append(fila)	
+
+
+
+	return lista2			
+
+
+			
+			
+
+
